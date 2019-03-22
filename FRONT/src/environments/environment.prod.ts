e90814edb5cc33908d5060a6f49fce0ab2d477c1
hdouangm <douangmala.hugo@gmail.com>
2019-03-22 16:24:08 +0100

    changing environment var

--- FRONT/src/environments/environment.prod.ts
+++ FRONT/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080/Patient-1.0-SNAPSHOT/rs'
+  apiUrl: 'http://e-ling.fr:8080/Patient-1.0-SNAPSHOT/rs'
 
 };
--- FRONT/src/environments/environment.ts
+++ FRONT/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/Patient-1.0-SNAPSHOT/rs'
+  apiUrl: 'http://e-ling.fr:8080/Patient-1.0-SNAPSHOT/rs'
 
 };
 
