78f1c5983eb432c9cab272b360df9b4b257e935b
diallorokiatou <40149545+diallorokiatou@users.noreply.github.com>
2019-03-24 20:31:50 +0100

    Add files via upload

---
+++ BACK/src/main/java/application/security/utils/Role.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package application.security.utils;
+
+/**
+ *
+ * @author asus
+ */
+
+/*  1 -> MEDECIN, -> 
+    2 -> //SECRETAIRE_MEDICAL,
+    3 -> INFIRMIERE,
+    4 -> LABORATIN,
+    5 -> DATAMANAGER
+*/
+public enum Role {
+   MEDECIN, 
+   SECRETAIRE_MEDICAL,
+   INFIRMIERE,
+   LABORATIN,
+   DATAMANAGER
+    
+}
