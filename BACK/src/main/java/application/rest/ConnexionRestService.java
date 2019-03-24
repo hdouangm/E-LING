3d1f2f589bb90a8fd593e7f1805587da89ace3a0
kakashisenc <aceforu@hotmail.fr>
2019-03-24 21:33:49 +0100

    fix connexion

--- BACK/src/main/java/application/rest/ConnexionRestService.java
+++ BACK/src/main/java/application/rest/ConnexionRestService.java
@@ -2,6 +2,7 @@ package application.rest;
 
 import application.eling.domain.CompteAphp;
 import application.eling.domain.Credential;
+import application.eling.domain.DonneesSociales;
 import application.eling.domain.Employe;
 import application.repository.CompteAphpRepository;
 import application.repository.EmployeRepository;
