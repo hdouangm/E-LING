3e4e721f6b83c72b1bad6d2b395496d209f9913f
Raphael B <39297970+Rafal272@users.noreply.github.com>
2019-03-25 03:11:53 +0100

    Update CompteAphpSetup.java

--- BACK/src/main/java/application/repository/CompteAphpSetup.java
+++ BACK/src/main/java/application/repository/CompteAphpSetup.java
@@ -21,14 +21,14 @@ public class CompteAphpSetup {
     @PostConstruct
     private void createData() {
        repository.save(new CompteAphp("Rayane@gmail.com", "la","FR",new Employe(null, null, new DonneesSociales("Cherifi","Rayane",null,null,null,null,null,null), 0,null),"nuv6q5n0djmrd650vv08u4b0vo@group.calendar.google.com"));
-       repository.save(new CompteAphp("Hugo@gmail.com", "la","FR",new Employe(null, null, new DonneesSociales("Douangmala","Hugo",null,null,null,null,null,null), 0,null),"qopic6kffqk2lgnjtti78ud1go@group.calendar.google.com"));
-
-       repository.save(new CompteAphp("Cristiano@gmail.com", "la","FR",new Employe(null, null, new DonneesSociales("Dos Santos","Cristiano",null,null,null,null,null,null), 1,null),"qopic6kffqk2lgnjtti78ud1go@group.calendar.google.com"));
-       repository.save(new CompteAphp("Lionel@gmail.com", "la","FR",new Employe(null, null, new DonneesSociales("Rodriguez","Lionel",null,null,null,null,null,null), 1,null),"qopic6kffqk2lgnjtti78ud1go@group.calendar.google.com"));
-
-       repository.save(new CompteAphp("Serge@gmail.com", "la","FR",new Employe(null, null, new DonneesSociales("Durand","Serge",null,null,null,null,null,null), 2,null),"qopic6kffqk2lgnjtti78ud1go@group.calendar.google.com"));
-       repository.save(new CompteAphp("Robert@gmail.com", "la","FR",new Employe(null, null, new DonneesSociales("Petit","Robert",null,null,null,null,null,null), 2,null),"qopic6kffqk2lgnjtti78ud1go@group.calendar.google.com"));
-
+       repository.save(new CompteAphp("Robert@gmail.com", "la","FR",new Employe(null, null, new DonneesSociales("Petit","Robert",null,null,null,null,null,null), 0,null),"qopic6kffqk2lgnjtti78ud1go@group.calendar.google.com"));
+        
+       repository.save(new CompteAphp("Hugo@gmail.com", "la","FR",new Employe(null, null, new DonneesSociales("Douangmala","Hugo",null,null,null,null,null,null), 1,null),"qopic6kffqk2lgnjtti78ud1go@group.calendar.google.com"));
+       repository.save(new CompteAphp("Serge@gmail.com", "la","FR",new Employe(null, null, new DonneesSociales("Durand","Serge",null,null,null,null,null,null), 1,null),"qopic6kffqk2lgnjtti78ud1go@group.calendar.google.com"));
+    
+       repository.save(new CompteAphp("Cristiano@gmail.com", "la","FR",new Employe(null, null, new DonneesSociales("Dos Santos","Cristiano",null,null,null,null,null,null), 2,null),"qopic6kffqk2lgnjtti78ud1go@group.calendar.google.com"));
+       repository.save(new CompteAphp("Lionel@gmail.com", "la","FR",new Employe(null, null, new DonneesSociales("Rodriguez","Lionel",null,null,null,null,null,null), 2,null),"qopic6kffqk2lgnjtti78ud1go@group.calendar.google.com"));
+        
        repository.save(new CompteAphp("Anne@gmail.com", "la","FR",new Employe(null, null, new DonneesSociales("Girard","Anne",null,null,null,null,null,null), 3,null),"qopic6kffqk2lgnjtti78ud1go@group.calendar.google.com"));
        repository.save(new CompteAphp("Roger@gmail.com", "la","FR",new Employe(null, null, new DonneesSociales("Leroy","Roger",null,null,null,null,null,null), 3,null),"qopic6kffqk2lgnjtti78ud1go@group.calendar.google.com"));
 
