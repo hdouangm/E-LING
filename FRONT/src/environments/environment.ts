fd66197015d297ae6b1f06d7075345cee9a352a0
kakashisenc <aceforu@hotmail.fr>
2019-03-28 02:43:46 +0100

    d

--- FRONT/src/environments/environment.ts
+++ FRONT/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  // apiUrl: 'http://e-ling.fr:8080/Patient-1.0-SNAPSHOT/rs'
-  apiUrl : 'http://localhost:8080/Patient-1.0-SNAPSHOT/rs'
+   apiUrl: 'http://e-ling.fr:8080/Patient-1.0-SNAPSHOT/rs'
+  //apiUrl : 'http://localhost:8080/Patient-1.0-SNAPSHOT/rs'
 };
 
 /*
